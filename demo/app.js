58edf08d147dbaeb297bd76e5ca1aa9dfb8cf64f
Jakob Voss <voss@gbv.de>
2014-08-15 10:47:21 +0200

    removed mapping and occurrences

--- demo/app.js
+++ demo/app.js
@@ -27,26 +27,12 @@ function myController($scope, SkosConceptProvider) {
 
 cocodaDemo.run(function($rootScope,$http) {
 
-    $rootScope.mappingSample = {};
-    $http.get('data/mapping-1.json').success(function(data){
-        $rootScope.mappingSample = data;
-	});
-
-	$rootScope.occurrencesSample = {};
-    $http.get('data/occurrences-1.json').success(function(data){
-        $rootScope.occurrencesSample = data;
-    });
-
     $rootScope.treeSample = {};
 	$http.get('data/tree-1.json').success(function(data){
         $rootScope.treeSample = data;
 	});
-
-    $rootScope.searchSample = {
-        // TODO
-    };
-
 });
+
 cocodaDemo.config(function($locationProvider) {
     $locationProvider.html5Mode(true);
 }).controller('MainCtrl', function ($$rootScope, $location) { });
