cee3c3abcd05a6d6001b7c7e2f6ab32ff9f16c69
Jakob Voss <voss@gbv.de>
2014-09-04 14:05:34 +0200

    started skos-concept-browser

---
+++ src/directives/skosConceptBrowser.js
@@ -0,0 +1,35 @@
+/**
+ * @ngdoc directive
+ * @name ng-skos.directive:skosConceptBrowser
+ * @restrict E
+ * @scope
+ * @description
+ *
+ * ## Scope
+ *
+ * The following variables are added to the scope:
+ * <ul>
+ * <li>concept
+ * </ul>
+ *
+ * @param {string} concept selected [concept](#/guide/concepts)
+ * @param {string} template-url URL of a template to display the concept browser
+ */
+angular.module('ngSKOS')
+.directive('skosConceptBrowser', function() {
+    return {
+        restrict: 'E',
+        scope: { 
+            concept: '=concept',
+        },
+        templateUrl: function(elem, attrs) {
+            return attrs.templateUrl ? 
+                   attrs.templateUrl : 'src/templates/skos-concept-browser.html';
+        },
+        link: function link(scope, element, attr) {
+            scope.$watch('concept',function(concept) {
+                /* ... */
+            },true);
+        }
+     }
+);
