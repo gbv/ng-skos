4a84a0dd60d2e0238f3ba6c23f4cfb70948d4ec0
Jakob Voss <voss@gbv.de>
2014-09-04 14:35:03 +0200

    template source

--- src/directives/skosConceptBrowser.js
+++ src/directives/skosConceptBrowser.js
@@ -13,6 +13,9 @@
  * </ul>
  *
  * @param {string} concept selected [concept](#/guide/concepts)
+ * @param {string} typeahead OpenSearchSuggestions for typeahead
+ * @param {string} lookup-notation SkosConceptProvider to look up by notation
+ * @param {string} search NOT SUPPORTED YET
  * @param {string} template-url URL of a template to display the concept browser
  */
 angular.module('ngSKOS')
@@ -21,15 +24,34 @@ angular.module('ngSKOS')
         restrict: 'E',
         scope: { 
             concept: '=concept',
+            typeahead: '=typeahead',
+            lookupNotation: '=lookupNotation',
+            search: '=search', // TODO
         },
         templateUrl: function(elem, attrs) {
             return attrs.templateUrl ? 
-                   attrs.templateUrl : 'src/templates/skos-concept-browser.html';
+                   attrs.templateUrl : 'template/skos-concept-browser.html';
         },
         link: function link(scope, element, attr) {
+            scope.searchLabel = "";
             scope.$watch('concept',function(concept) {
                 /* ... */
             },true);
+
+            // look up by notation or label
+            scope.lookupConcept = function(item) {
+                // populate with basic data
+                var concept = {
+                    notation: [ item.notation ],
+                    prefLabel: { // TODO: configure language
+                        de: item.label
+                    }
+                };
+                // call SkosConceptProvider
+                scope.lookup.updateConcept(concept).then(function() {
+                    console.log("updateConcept");
+                });
+            };
         }
      }
 );
