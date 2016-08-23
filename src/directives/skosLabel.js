080ff2c7f0cd59a6fb9fd7e0d630451b579ea59e
hornmo <moritz.horn@gmail.com>
2016-08-24 01:07:58 +0200

    working on skos-label functionality

--- src/directives/skosLabel.js
+++ src/directives/skosLabel.js
@@ -5,7 +5,7 @@
  * @description
  *
  * Shows the multilingual label of a concept or concept scheme, given as
- * JSON object with language codes mapped to labels. A preferred language 
+ * JSON object with language codes mapped to labels. One or more preferred languages 
  * can be selected with parameter `lang`. The language a label is actually
  * shown in, is made available as element attribute `skos-lang`.
  *
@@ -13,8 +13,9 @@
  * to select an alternative languages.
  *
  * @param {string} skos-label Expression with multilingual label data
- * @param {string=} lang preferred language to show. If not available,
- *      a language is chosen based on the given label data.
+ * @param {string=|array=} lang preferred language(s) to show. If several languages 
+ *      are provided via an array, they are processed in that order. If none are 
+ *      available, a language is chosen based on the given label data.
  *
  * @example
  <example module="myApp">
@@ -61,35 +62,66 @@ angular.module('ngSKOS')
         template: '{{label[language]}}',
         link: function(scope, element, attrs) {
 
-            function updateLanguage(language) {
-                scope.language = language ? language : attrs.lang;
-
-                language = scope.label ? selectLanguage(scope.label, scope.language) : "";
-
-                if (language != scope.language) {
-                    scope.language = language;
-                }
-
-                attrs.$set('skos-lang', language);
+            function updateLanguage(language) {            
+              scope.language = language ? language : attrs.lang;
+              language = scope.label ? selectLanguage(scope.label, scope.language) : "";
+              if (language != scope.language) {
+                  scope.language = language;
+              }
+              attrs.$set('skos-lang', language);
             }
 
-            function selectLanguage(labels, language) {
-                if ( angular.isObject(labels) ) {
-                    if ( language && angular.isString(labels[language])) {
-                        return language;
-                    } else {
-                        return guessLanguage(labels);
-                    }
+            function selectLanguage(labels, languages) {
+              if ( angular.isObject(labels) ) {
+                if (languages && languages.indexOf(",") !== -1){
+                  languages = languages.split(",");
                 }
+                if (angular.isString(languages) && angular.isString(labels[languages])){
+                  return languages;
+                }else if (angular.isArray(languages) && angular.isString(labels[languages[0]])){
+                    return languages[0];
+                } else {
+                    return guessLanguage(labels, languages);
+                }    
+              }
             }
 
-            function guessLanguage(labels) {
-                // TODO: https://github.com/gbv/ng-skos/issues/17
-                for (var language in labels) {
-                    if (angular.isString(labels[language])) {
-                        return language; // take arbitrary language
+            function guessLanguage(labels, languages) {      
+              if(languages){
+                if(angular.isString(languages)){
+
+                  for (var dialect in labels){
+                    if (dialect.indexOf(languages) !== -1){
+                      return dialect;
                     }
+                  }
+                  for (var language in labels){
+                    if (angular.isString(labels[language])){
+                      return language; // take arbitrary language
+                    }
+                  }
+                } else if (angular.isArray(languages)){
+                  for (i in languages){
+                    for (var dialect in labels){
+                      if (dialect.indexOf(languages[i]) !== -1){
+                        return dialect;
+                      }
+                    }
+                  };
+                  for (var language in labels){
+                    if (angular.isString(labels[language])){
+                      console.log(labels);
+                      return language; // take arbitrary language
+                    }
+                  }
                 }
+              }else{
+                for (var language in labels){
+                  if (angular.isString(labels[language])){
+                    return language; // take arbitrary language
+                  }
+                }
+              }
             }
 
             // update if lang attribute changed (also called at initialization)
